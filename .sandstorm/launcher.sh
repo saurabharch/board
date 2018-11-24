4112420f0f6d5a60cc1e1e28affca0cc65c40cdd
JJ <jan@jambor.pro>
2018-11-24 13:12:29 +0100

    Improvements

--- .sandstorm/launcher.sh
+++ .sandstorm/launcher.sh
@@ -1,33 +1,19 @@
 #!/bin/bash
 set -euo pipefail
-# This script is run every time an instance of our app - aka grain - starts up.
-# This is the entry point for your application both when a grain is first launched
-# and when a grain resumes after being previously shut down.
-#
-# This script is responsible for launching everything your app needs to run.  The
-# thing it should do *last* is:
-#
-#   * Start a process in the foreground listening on port 8000 for HTTP requests.
-#
-# This is how you indicate to the platform that your application is up and
-# ready to receive requests.  Often, this will be something like nginx serving
-# static files and reverse proxying for some other dynamic backend service.
-#
-# Other things you probably want to do in this script include:
-#
-#   * Building folder structures in /var.  /var is the only non-tmpfs folder
-#     mounted read-write in the sandbox, and when a grain is first launched, it
-#     will start out empty.  It will persist between runs of the same grain, but
-#     be unique per app instance.  That is, two instances of the same app have
-#     separate instances of /var.
-#   * Preparing a database and running migrations.  As your package changes
-#     over time and you release updates, you will need to deal with migrating
-#     data from previous schema versions to new ones, since users should not have
-#     to think about such things.
-#   * Launching other daemons your app needs (e.g. mysqld, redis-server, etc.)
 
-# By default, this script does nothing.  You'll have to modify it as
-# appropriate for your application.
-cd /opt/app
+mkdir -p /var/lib/nginx
+mkdir -p /var/log/nginx
+mkdir -p /var/run
 
-exit 0
+# Spawn mysqld, php
+/usr/sbin/php-fpm7.2 --fpm-config /etc/php/7.2/fpm/php-fpm.conf -c /etc/php/7.2/fpm/php.ini
+while [ ! -e /var/run/php5-fpm.sock ] ; do
+    echo "waiting for php5-fpm to be available at /var/run/php5-fpm.sock"
+    sleep .2
+done
+echo "started php-fpm. status code:" $?
+
+/usr/sbin/nginx -g "pid /var/run/nginx.pid;"
+echo "started nginx. status code:" $?
+
+sleep infinity
--- .sandstorm/setup.sh
+++ .sandstorm/setup.sh
@@ -1,45 +1,41 @@
 #!/bin/bash
+
+# When you change this file, you must take manual action. Read this doc:
+# - https://docs.sandstorm.io/en/latest/vagrant-spk/customizing/#setupsh
+
 set -euo pipefail
+# This is the ideal place to do things like:
+#
+export DEBIAN_FRONTEND=noninteractive
 
-# Update OS & install curl & unzip
-apt-get update -y
-apt-get install -y curl unzip
+# switch to debian testing
+cat <<EOF > /etc/apt/sources.list
+#------------------------------------------------------------------------------#
+#                   OFFICIAL DEBIAN REPOS
+#------------------------------------------------------------------------------#
 
-# Find latest Restyaboard version
-RESTYABOARD_VERSION=$(curl --silent https://api.github.com/repos/RestyaPlatform/board/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+###### Debian Main Repos
+deb http://deb.debian.org/debian/ testing main contrib non-free
+deb-src http://deb.debian.org/debian/ testing main contrib non-free
 
-# Initialize directory variables
-DOWNLOAD_DIR=/opt/restyaboard
-RESTYABOARD_DIR=/usr/share/nginx/html/restyaboard
+deb http://deb.debian.org/debian/ testing-updates main contrib non-free
+deb-src http://deb.debian.org/debian/ testing-updates main contrib non-free
 
-# Restyaboard DB details
+deb http://deb.debian.org/debian-security testing/updates main
+deb-src http://deb.debian.org/debian-security testing/updates main
+EOF
+
+# update os & install required packages
+apt-get update
+apt-get install -y curl unzip nginx php7.2-fpm php7.2-cli php7.2-curl php7.2-opcache php7.2-common php7.2-gd libpq5 php7.2-pgsql php7.2-mbstring php7.2-ldap imagemagick php-imagick php7.2-imap php7.2-xml postgresql gcc make autoconf libc-dev pkg-config php-geoip php7.2-dev libgeoip-dev
+
+# postgresql installation
 POSTGRES_DBHOST=localhost
 POSTGRES_DBNAME=restyaboard
 POSTGRES_DBUSER=restya
 POSTGRES_DBPASS=hjVl2!rGd
 POSTGRES_DBPORT=5432
-
-# PHP latest version package
-sh -c 'echo "deb http://ftp.de.debian.org/debian jessie main" > /etc/apt/sources.list.d/debjessie.list'
-apt install apt-transport-https lsb-release ca-certificates -y
-wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
-echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/php.list
-apt install debian-keyring debian-archive-keyring -y
-apt update -y
-
-# nginx installation
-apt install -y cron nginx
-service nginx start
-
-# php installation
-apt install -y php7.2 php7.2-opcache php7.2-common php7.2-fpm php7.2-cli php7.2-gd php7.2-curl libpq5 php7.2-pgsql php7.2-mbstring php7.2-ldap gcc imagemagick php7.2-imagick php7.2-imap php7.2-xml --allow-unauthenticated
-timezone=$(cat /etc/timezone)
-sed -i -e 's/date.timezone/;date.timezone/g' /etc/php/7.2/fpm/php.ini
-service php7.2-fpm start
-
-# postgresql installation
-apt install -y postgresql
-PSQL_VERSION=$(psql --version | egrep -o '[0-9]{1,}\.[0-9]{1,}' | head -1)
+PSQL_VERSION=$(psql --version | egrep -o '[0-9]{1,}' | head -1)
 sed -e 's/peer/trust/g' -e 's/ident/trust/g' < /etc/postgresql/${PSQL_VERSION}/main/pg_hba.conf > /etc/postgresql/${PSQL_VERSION}/main/pg_hba.conf.1
 cd /etc/postgresql/${PSQL_VERSION}/main || exit
 mv pg_hba.conf pg_hba.conf_old
@@ -47,37 +43,52 @@ mv pg_hba.conf.1 pg_hba.conf
 service postgresql restart
 
 # php geoip installation
-apt-get -y install gcc make autoconf libc-dev pkg-config php7.2-geoip php7.2-dev libgeoip-dev
-cd /opt/
-wget http://pecl.php.net/get/geoip-1.1.1.tgz
+cd /opt/app
+
+#
+# I'm getting an error with wget here:
+#  default: --2018-11-24 08:16:23--  https://pecl.php.net/get/geoip-1.1.1.tgz
+#  default: Resolving pecl.php.net (pecl.php.net)...
+#  default: 104.236.228.160
+#  default: Connecting to pecl.php.net (pecl.php.net)|104.236.228.160|:443...
+#  default: failed: Network is unreachable.
+# That's why I downloadad all gz files and put them in the app folder
+#
+#wget https://pecl.php.net/get/geoip-1.1.1.tgz
 tar zxvf ./geoip-1.1.1.tgz
-cd /opt/geoip-1.1.1/
+cd geoip-1.1.1
 phpize
 ls -la
 ./configure
 make
 make install
 echo "extension=geoip.so" >> /etc/php/7.2/fpm/php.ini
-wget http://geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz
-gunzip GeoIP.dat.gz
-mv GeoIP.dat /usr/share/GeoIP/GeoIP.dat
-wget http://geolite.maxmind.com/download/geoip/database/GeoIPv6.dat.gz
-gunzip GeoIPv6.dat.gz
-mv GeoIPv6.dat /usr/share/GeoIP/GeoIPv6.dat
-wget http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz
-gunzip GeoLiteCity.dat.gz
-mv GeoLiteCity.dat /usr/share/GeoIP/GeoIPCity.dat
-wget http://geolite.maxmind.com/download/geoip/database/GeoLiteCityv6-beta/GeoLiteCityv6.dat.gz
-gunzip GeoLiteCityv6.dat.gz
-mv GeoLiteCityv6.dat /usr/share/GeoIP/GeoLiteCityv6.dat
-wget http://download.maxmind.com/download/geoip/database/asnum/GeoIPASNum.dat.gz
-gunzip GeoIPASNum.dat.gz
-mv GeoIPASNum.dat /usr/share/GeoIP/GeoIPASNum.dat
-wget http://download.maxmind.com/download/geoip/database/asnum/GeoIPASNumv6.dat.gz
-gunzip GeoIPASNumv6.dat.gz
-mv GeoIPASNumv6.dat /usr/share/GeoIP/GeoIPASNumv6.dat
+cd /opt/app
+#wget http://geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz
+#gunzip GeoIP.dat.gz
+#mv GeoIP.dat /usr/share/GeoIP/GeoIP.dat
+#wget http://geolite.maxmind.com/download/geoip/database/GeoIPv6.dat.gz
+#gunzip GeoIPv6.dat.gz
+#mv GeoIPv6.dat /usr/share/GeoIP/GeoIPv6.dat
+#wget http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz
+#gunzip GeoLiteCity.dat.gz
+#mv GeoLiteCity.dat /usr/share/GeoIP/GeoIPCity.dat
+#wget http://geolite.maxmind.com/download/geoip/database/GeoLiteCityv6-beta/GeoLiteCityv6.dat.gz
+#gunzip GeoLiteCityv6.dat.gz
+#mv GeoLiteCityv6.dat /usr/share/GeoIP/GeoLiteCityv6.dat
+#wget http://download.maxmind.com/download/geoip/database/asnum/GeoIPASNum.dat.gz
+#gunzip GeoIPASNum.dat.gz
+#mv GeoIPASNum.dat /usr/share/GeoIP/GeoIPASNum.dat
+#wget http://download.maxmind.com/download/geoip/database/asnum/GeoIPASNumv6.dat.gz
+#gunzip GeoIPASNumv6.dat.gz
+#mv GeoIPASNumv6.dat /usr/share/GeoIP/GeoIPASNumv6.dat
 
 # Restyaboard script installation
+# Find latest Restyaboard version
+RESTYABOARD_VERSION=$(curl --silent https://api.github.com/repos/RestyaPlatform/board/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+# Initialize directory variables
+DOWNLOAD_DIR=/var/restyaboard
+RESTYABOARD_DIR=/opt/app
 mkdir -p ${DOWNLOAD_DIR}
 curl -v -L -G -d "app=board&ver=${RESTYABOARD_VERSION}" -o /tmp/restyaboard.zip http://restya.com/download.php
 unzip /tmp/restyaboard.zip -d ${DOWNLOAD_DIR}
@@ -86,13 +97,6 @@ mkdir -p ${RESTYABOARD_DIR}
 cp -r ${DOWNLOAD_DIR}/* ${RESTYABOARD_DIR}
 find ${RESTYABOARD_DIR} -type d -print0 | xargs -0 chmod 0755
 find ${RESTYABOARD_DIR} -type f -print0 | xargs -0 chmod 0644
-
-# Restyaboard nginx configuration
-cp ${DOWNLOAD_DIR}/restyaboard.conf /etc/nginx/conf.d
-sed -i "s|root.*html|root ${RESTYABOARD_DIR}|" /etc/nginx/conf.d/restyaboard.conf
-sed -i "s/server_name.*$/server_name _;/" /etc/nginx/conf.d/restyaboard.conf
-sed -i "s|listen 80.*$|listen 80;|" /etc/nginx/conf.d/restyaboard.conf
-rm -rf /etc/nginx/conf.d/default.conf /etc/nginx/sites-available/default.conf /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default.conf /etc/nginx/sites-enabled/default
 
 # Restyaboard DB creation
 psql -U postgres -c "\q"
@@ -109,16 +113,38 @@ sed -i "s/^.*'R_DB_PASSWORD'.*$/define('R_DB_PASSWORD', '${POSTGRES_DBPASS}');/g
 sed -i "s/^.*'R_DB_HOST'.*$/define('R_DB_HOST', '${POSTGRES_DBHOST}');/g" "${RESTYABOARD_DIR}/server/php/config.inc.php"
 sed -i "s/^.*'R_DB_PORT'.*$/define('R_DB_PORT', '${POSTGRES_DBPORT}');/g" "${RESTYABOARD_DIR}/server/php/config.inc.php"
 
-# Restyaboard cron setup
-echo "*/5 * * * * ${RESTYABOARD_DIR}/server/php/shell/instant_email_notification.sh > /dev/null 2> /dev/null" >> /var/spool/cron/crontabs/root
-echo "0 * * * * ${RESTYABOARD_DIR}/server/php/shell/periodic_email_notification.sh > /dev/null 2> /dev/null" >> /var/spool/cron/crontabs/root
-echo "*/30 * * * * ${RESTYABOARD_DIR}/server/php/shell/imap.sh > /dev/null 2> /dev/null" >> /var/spool/cron/crontabs/root
-echo "*/5 * * * * ${RESTYABOARD_DIR}/server/php/shell/webhook.sh > /dev/null 2> /dev/null" >> /var/spool/cron/crontabs/root
-echo "*/5 * * * * ${RESTYABOARD_DIR}/server/php/shell/card_due_notification.sh > /dev/null 2> /dev/null" >> /var/spool/cron/crontabs/root
+# Stopping Services
+service php7.2-fpm stop
+service nginx stop
+service postgresql stop
+systemctl disable php7.2-fpm
+systemctl disable nginx
+systemctl disable postgresql
 
-# Restarting services
-service cron restart
-service php7.2-fpm restart
-service nginx restart
+# patch /etc/php/7.2/fpm/pool.d/www.conf to not change uid/gid to www-data
+sed --in-place='' \
+        --expression='s/^listen.owner = www-data/;listen.owner = www-data/' \
+        --expression='s/^listen.group = www-data/;listen.group = www-data/' \
+        --expression='s/^user = www-data/;user = www-data/' \
+        --expression='s/^group = www-data/;group = www-data/' \
+        /etc/php/7.2/fpm/pool.d/www.conf
+# patch /etc/php/7.2/fpm/php-fpm.conf to not have a pidfile
+sed --in-place='' \
+        --expression='s/^pid =/;pid =/' \
+        /etc/php/7.2/fpm/php-fpm.conf
+# patch /etc/php/7.2/fpm/php-fpm.conf to place the sock file in /var
+sed --in-place='' \
+       --expression='s/^listen = \/run\/php\/php7.2-fpm.sock/listen = \/var\/run\/php\/php7.2-fpm.sock/' \
+       /etc/php/7.2/fpm/pool.d/www.conf
+# patch /etc/php/7.2/fpm/pool.d/www.conf to no clear environment variables
+# so we can pass in SANDSTORM=1 to apps
+sed --in-place='' \
+        --expression='s/^;clear_env = no/clear_env=no/' \
+        /etc/php/7.2/fpm/pool.d/www.conf
+# patch timezone
+timezone=$(cat /etc/timezone)
+sed --in-place='' \
+       --expression='s/date.timezone/;date.timezone/g' \
+       /etc/php/7.2/fpm/php.ini
 
 exit 0
