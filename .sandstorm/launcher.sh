861133d1d961562c3365777cc62de319fabe7ed1
JJ <jan@jambor.pro>
2018-11-26 08:02:10 +0100

    removed check

--- .sandstorm/launcher.sh
+++ .sandstorm/launcher.sh
@@ -7,10 +7,6 @@ mkdir -p /var/run
 
 # Spawn mysqld, php
 /usr/sbin/php-fpm7.2 --fpm-config /etc/php/7.2/fpm/php-fpm.conf -c /etc/php/7.2/fpm/php.ini
-while [ ! -e /var/run/php5-fpm.sock ] ; do
-    echo "waiting for php5-fpm to be available at /var/run/php5-fpm.sock"
-    sleep .2
-done
 echo "started php-fpm. status code:" $?
 
 /usr/sbin/nginx -g "pid /var/run/nginx.pid;"
